a24c9b6f2f0f1ba22e8ae7978e03e4f13b123e42
sbishop7 <seth.bishop@gmail.com>
2017-05-13 21:50:07 -0700

    updated Dojo Survey with validation

--- Python/Flask/DojoSurvey/server.py
+++ Python/Flask/DojoSurvey/server.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, session
 
 app = Flask(__name__)
+
+app.secret_key="piuq3bg[i"
 
 @app.route('/')
 
@@ -10,16 +12,21 @@ def index():
 @app.route('/result', methods=['POST'])
 
 def result():
-    print 'Got info'
-    name = request.form['name']
-    location = request.form['location']
-    language = request.form['language']
-    comment = request.form['comments']
-    return render_template('result.html', name=name, location=location, language=language, comment=comment)    
-    
-# @app.route('/results')
-
-# def results():
-#     return render_template('results.html')
+    print len(request.form['name'])
+    if len(request.form['name']) < 1:
+        flash("Name field cannot be blank")
+        return redirect('/')
+    if len(request.form['comments']) < 1:
+        flash("Comment field cannot be blank")
+        return redirect('/')
+    if len(request.form['comments']) > 120:
+        flash("Comment field is too long")
+        return redirect('/')
+    else:
+        name = request.form['name']
+        location = request.form['location']
+        language = request.form['language']
+        comment = request.form['comments']
+    return render_template('result.html', name=name, location=location, language=language, comment=comment)
 
 app.run(debug=True)
