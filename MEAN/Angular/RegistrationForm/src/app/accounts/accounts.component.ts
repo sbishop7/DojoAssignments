2672fd5621aa184f914c7b530b38c6b6b05844b6
sbishop7 <seth.bishop@gmail.com>
2017-06-15 08:49:57 -0700

    updated Registration Form in MEAN

--- MEAN/Angular/RegistrationForm/src/app/accounts/accounts.component.ts
+++ MEAN/Angular/RegistrationForm/src/app/accounts/accounts.component.ts
@@ -12,12 +12,14 @@ export class AccountsComponent implements OnInit {
         lastName: "",
         email: "",
         password: "",
+        confirmPW: "",
         address: "",
         unit: "",
         city: "",
         state: "",
-        lucky: false
+        lucky: ""
       }
+  message: string = ""
   
   states: Array<string> = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
@@ -31,11 +33,12 @@ export class AccountsComponent implements OnInit {
         lastName: "",
         email: "",
         password: "",
+        confirmPW: "",
         address: "",
         unit: "",
         city: "",
         state: "",
-        lucky: false
+        lucky: ""
       }
   }
 
