5dec42e26c9b9b9f44a97907d695aa164f7e7f55
sbishop7 <seth.bishop@gmail.com>
2017-06-22 16:56:21 -0700

    finished Login and Registration

--- MEAN/FullMEAN/LoginAndRegistration/client/src/app/app-routing.module.ts
+++ MEAN/FullMEAN/LoginAndRegistration/client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UsersComponent } from './users/users.component';
+import { ListUsersComponent } from './users/list-users/list-users.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', pathMatch: "full", redirectTo: '/login' },
+  { path: 'login', component: UsersComponent },
+  { path: 'users', component: ListUsersComponent}
 ];
 
 @NgModule({
--- MEAN/FullMEAN/LoginAndRegistration/client/src/app/app.module.ts
+++ MEAN/FullMEAN/LoginAndRegistration/client/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserService } from './users/user.service';
+import { ListUsersComponent } from './users/list-users/list-users.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    ListUsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
